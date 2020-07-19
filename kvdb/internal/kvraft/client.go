f608ffe97b22f2e7c62063d1ee9e04dcda66f62b
Jian Yuan <jian_yuan@claris.com>
2020-07-18 21:34:49 -0700

    Add solution to lab 3A

--- kvdb/internal/kvraft/client.go
+++ kvdb/internal/kvraft/client.go
@@ -4,11 +4,18 @@ import (
 	"crypto/rand"
 	"kvdb/internal/labrpc"
 	"math/big"
+	"strconv"
+	"sync/atomic"
+
+	"github.com/google/uuid"
 )
 
 type Clerk struct {
 	servers []*labrpc.ClientEnd
 	// You will have to modify this struct.
+	id     string
+	seq    int64
+	leader int32
 }
 
 func nrand() int64 {
@@ -22,6 +29,8 @@ func MakeClerk(servers []*labrpc.ClientEnd) *Clerk {
 	ck := new(Clerk)
 	ck.servers = servers
 	// You'll have to add code here.
+	ck.id = uuid.New().String()
+	ck.leader = 0
 	return ck
 }
 
@@ -38,9 +47,24 @@ func MakeClerk(servers []*labrpc.ClientEnd) *Clerk {
 // arguments. and reply must be passed as a pointer.
 //
 func (ck *Clerk) Get(key string) string {
+	N := int32(len(ck.servers))
+	i := atomic.LoadInt32(&ck.leader)
+	args := GetArgs{
+		Key:       key,
+		CommandID: ck.id + "-" + strconv.FormatInt(atomic.AddInt64(&ck.seq, 1), 10),
+	}
 
-	// You will have to modify this function.
-	return ""
+	for {
+		var reply GetReply
+		if ok := ck.servers[i].Call("KVServer.Get", &args, &reply); !ok || reply.Err == ErrWrongLeader {
+			i = (i + 1) % N
+			continue
+		}
+		atomic.StoreInt32(&ck.leader, int32(i))
+		if reply.Err == OK || reply.Err == ErrNoKey {
+			return reply.Value
+		}
+	}
 }
 
 //
@@ -55,6 +79,26 @@ func (ck *Clerk) Get(key string) string {
 //
 func (ck *Clerk) PutAppend(key string, value string, op string) {
 	// You will have to modify this function.
+	N := int32(len(ck.servers))
+	i := atomic.LoadInt32(&ck.leader)
+	args := PutAppendArgs{
+		Key:       key,
+		Value:     value,
+		Op:        op,
+		CommandID: ck.id + "-" + strconv.FormatInt(atomic.AddInt64(&ck.seq, 1), 10),
+	}
+
+	for {
+		var reply PutAppendReply
+		if ok := ck.servers[i].Call("KVServer.PutAppend", &args, &reply); !ok || reply.Err == ErrWrongLeader {
+			i = (i + 1) % N
+			continue
+		}
+		atomic.StoreInt32(&ck.leader, i)
+		if reply.Err == OK {
+			return
+		}
+	}
 }
 
 func (ck *Clerk) Put(key string, value string) {
